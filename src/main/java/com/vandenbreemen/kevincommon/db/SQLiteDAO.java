32704a85168cd6c06a97ada25e4095a141dae9b0
kevin <kevin.vandenbreemen@gmail.com>
2021-10-20 19:16:52 -0400

    Refactoring method API

--- src/main/java/com/vandenbreemen/kevincommon/db/SQLiteDAO.java
+++ src/main/java/com/vandenbreemen/kevincommon/db/SQLiteDAO.java
@@ -24,6 +24,22 @@ public class SQLiteDAO {
         }
     }
 
+    public void executeInsert(String sql, Object[] values) {
+        try(Connection connection = DriverManager.getConnection("jdbc:sqlite:" + filePath); PreparedStatement statement = connection.prepareStatement(sql);) {
+
+            int index = 1;
+            for(Object v : values) {
+                setValueAtStatementIndex(statement, index, v);
+                index++;
+            }
+
+            statement.executeUpdate();
+
+        } catch (Exception ex) {
+            logger.error("Could not perform insert\n"+sql, ex);
+        }
+    }
+
     public void insert(String tableName, String[] columnNames, Object[] values) {
 
         StringBuilder prepared = new StringBuilder("INSERT INTO ").append(tableName).append(" ( ").append(String.join(", ", columnNames)).append(")");
@@ -37,19 +53,7 @@ public class SQLiteDAO {
         }
         prepared.append(")");
 
-        try(Connection connection = DriverManager.getConnection("jdbc:sqlite:" + filePath); PreparedStatement statement = connection.prepareStatement(prepared.toString());) {
-
-            int index = 1;
-            for(Object v : values) {
-                setValueAtStatementIndex(statement, index, v);
-                index++;
-            }
-
-            statement.executeUpdate();
-
-        } catch (Exception ex) {
-            logger.error("Could not perform insert\n"+prepared, ex);
-        }
+        executeInsert(prepared.toString(), values);
     }
 
     private void setValueAtStatementIndex(PreparedStatement statement, int index, Object v) throws SQLException {
@@ -61,31 +65,41 @@ public class SQLiteDAO {
         }
     }
 
-    public List<Map<String, Object>> query(String tableName, String[] columns, String columnName, Object value) {
-
-        StringBuilder prepared = new StringBuilder("SELECT ").append(String.join(", ", columns)).append(" FROM ").append(tableName).append(" WHERE ").append(columnName).append(" = ?");
-
-
-        try(Connection connection = DriverManager.getConnection("jdbc:sqlite:" + filePath); PreparedStatement statement = connection.prepareStatement(prepared.toString());) {
-            setValueAtStatementIndex(statement, 1, value);
+    public List<Map<String, Object>> query(String sql, Object[] values) {
+        try(Connection connection = DriverManager.getConnection("jdbc:sqlite:" + filePath); PreparedStatement statement = connection.prepareStatement(sql);) {
+            int index = 1;
+            for(Object v : values) {
+                setValueAtStatementIndex(statement, index, v);
+                index++;
+            }
             ResultSet rs = statement.executeQuery();
 
+            String[] columnsToFetch = new String[rs.getMetaData().getColumnCount()];
+            for(int i=0; i<columnsToFetch.length; i++) {
+                columnsToFetch[i] = rs.getMetaData().getColumnName(i+1);
+            }
+
             ArrayList<Map<String, Object>> result = new ArrayList<>();
-            while(rs.next())
-            {
+            while(rs.next()) {
                 Map<String, Object> row = new HashMap<>();
-                for(String column : columns) {
+                for(String column : columnsToFetch) {
                     row.put(column, rs.getObject(column));
                 }
                 result.add(row);
-
             }
 
             return result;
         }catch (Exception ex) {
-            logger.error("Could not perform query\n"+prepared, ex);
+            logger.error("Could not perform query\n"+sql, ex);
         }
         return Collections.emptyList();
+    }
+
+    public List<Map<String, Object>> query(String tableName, String[] columns, String columnName, Object value) {
+
+        StringBuilder prepared = new StringBuilder("SELECT ").append(String.join(", ", columns)).append(" FROM ").append(tableName).append(" WHERE ").append(columnName).append(" = ?");
+
+        return query(prepared.toString(), new Object[]{value});
     }
 
     public void update(String tableName, String[] columns, Object[] newValues, String whereColumn, Object equalsValue) {
